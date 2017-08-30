5d3353bd72efbfc4fd2bb6c6e93bbe968e72f4a3
lymanjohnson <lyman.johnson@gmail.com>
2017-08-30 11:06:59 -0400

    Progress

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const mustacheExpress = require('mustache-express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const Box = require("./models/box.js");
+
+const DUPLICATE_RECORD_ERROR = 11000;
+
+const mongoURL = 'mongodb://localhost:27017/recipes';
+mongoose.connect(mongoURL, {useMongoClient: true});
 mongoose.Promise = require('bluebird');
-// Replace "test" with your database name.
-mongoose.connect('mongodb://localhost:27017/test');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('mustache', mustacheExpress());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache')
+
+/*  WHAT ARE THESE?  */
+
+// app.set('layout', 'layout');
+// app.use('/static', express.static('static'));
+
+/*                   */
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/new/')
+  res.render("new_box")
